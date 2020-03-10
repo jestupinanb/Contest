75e27d5c4eff3076bbf57a318200bc4f75815da7
Jaime Eduardo <30636279+jestupinanb@users.noreply.github.com>
2020-03-10 08:07:10 -0500

    Update Journey Planning.cpp

--- Random Contest/Journey Planning.cpp
+++ Random Contest/Journey Planning.cpp
@@ -3,23 +3,23 @@
 #include <vector>
 
 long long lcs(std::vector<int> b, int t, int i, int n) {
-	long long max = 0;
-	if (i == n) return 0;
-	if (t == -1) return std::max(lcs(b, t, i + 1, n), b[i] + lcs(b, i, i + 1, n));
-	if (t - i != b[t] - b[i])
-		max = b[i]+lcs(b, i, i + 1, n);
-	long long temp = lcs(b, t, i + 1, n);
-	if (temp > max) max = temp;
-	return max;
+    long long max = 0;
+    if (i == n) return 0;
+    if (t == -1) return std::max(lcs(b, t, i + 1, n), b[i] + lcs(b, i, i + 1, n));
+    if (i - t  == b[i] - b[t])
+        max = b[i]+lcs(b, i, i + 1, n);
+    long long temp = lcs(b, t, i + 1, n);
+    if (temp > max) max = temp;
+    return max;
 }
 
 int main()
 {
-	int n;
-	std::cin >> n;
-	std::vector<int> b(n);
-	for (int i = 0; i < n; i++) {
-		std::cin >> b[i];
-	}
-	std::cout << lcs(b, -1, 0, n);
+    int n;
+    std::cin >> n;
+    std::vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> b[i];
+    }
+    std::cout << lcs(b, -1, 0, n);
 }
