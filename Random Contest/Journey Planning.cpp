5280c758efddbf2956f74b69ed4bceddeabd34c0
Jaime Eduardo <30636279+jestupinanb@users.noreply.github.com>
2020-03-10 15:58:47 -0500

    Update Journey Planning.cpp

--- Random Contest/Journey Planning.cpp
+++ Random Contest/Journey Planning.cpp
@@ -2,14 +2,21 @@
 #include <bits/stdc++.h>
 #include <vector>
 
+std::vector<std::vector<int>> dp;
+
 long long lcs(std::vector<int> b, int t, int i, int n) {
     long long max = 0;
     if (i == n) return 0;
-    if (t == -1) return std::max(lcs(b, t, i + 1, n), b[i] + lcs(b, i, i + 1, n));
+    if(dp[t][i] != -1) return dp[t][i];
+    if (t == n){
+        dp[t][i] = std::max(lcs(b, t, i + 1, n), b[i] + lcs(b, i, i + 1, n));
+        return dp[t][i];
+    }
     if (i - t  == b[i] - b[t])
         max = b[i]+lcs(b, i, i + 1, n);
     long long temp = lcs(b, t, i + 1, n);
     if (temp > max) max = temp;
+    dp[t][i] = max;
     return max;
 }
 
@@ -17,9 +24,10 @@ int main()
 {
     int n;
     std::cin >> n;
+    dp.resize(n+1,std::vector<int>(n,-1));
     std::vector<int> b(n);
     for (int i = 0; i < n; i++) {
         std::cin >> b[i];
     }
-    std::cout << lcs(b, -1, 0, n);
+    std::cout << lcs(b, n, 0, n);
 }
